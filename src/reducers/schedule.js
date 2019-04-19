46ee0014f1d206e303fae3ab7f2e0c923159df9d
Ernest <iattempt.net@gmail.com>
2019-04-19 14:00:55 +0000

    Add test for ISO 8601

--- src/reducers/schedule.js
+++ src/reducers/schedule.js
@@ -6,18 +6,28 @@ const default_state = {
   [KEYS.originalSchedules]: []
 };
 
-const convertToTimestamp = time_string => {
+export const omitSecondForTimestamp = timestamp => {
+  return (timestamp - timestamp % 60) / 60
+};
+
+export const convertToTimestamp = time_string => {
+  time_string = time_string.replace(/:/g, '')
+  while (time_string.length < 6) {
+    time_string += '0'
+  }
   return (
-    parseInt(time_string.slice(0, 2)) * 60 + parseInt(time_string.slice(3, 5))
+    parseInt(time_string.slice(0, 2)) * 60 * 60 +
+    parseInt(time_string.slice(2, 4)) * 60 +
+    parseInt(time_string.slice(4, 6))
   );
 };
 
-const convertDepartureToTimestamp = departure => {
-  return convertToTimestamp(departure);
+export const convertDepartureToTimestamp = departure => {
+  return omitSecondForTimestamp(convertToTimestamp(departure));
 };
 
-const convertArrivalToTimestamp = (arrival, departure) => {
-  let arrival_timestamp = convertToTimestamp(arrival);
+export const convertArrivalToTimestamp = (arrival, departure) => {
+  let arrival_timestamp = omitSecondForTimestamp(convertToTimestamp(arrival));
 
   if (departure !== undefined) {
     const departure_timestamp = convertDepartureToTimestamp(departure);
@@ -74,8 +84,8 @@ export default (state = default_state, action) => {
             break;
 
           case KEYS.duration:
-            const a_duration = convertToTimestamp(a.duration);
-            const b_duration = convertToTimestamp(b.duration);
+            const a_duration = omitSecondForTimestamp(convertToTimestamp(a.duration));
+            const b_duration = omitSecondForTimestamp(convertToTimestamp(b.duration));
             flag = a_duration - b_duration;
             break;
 
---
+++ src/reducers/test/schedule.test.js
@@ -0,0 +1,100 @@
+import {
+  omitSecondForTimestamp,
+  convertToTimestamp,
+} from '../schedule'
+
+describe('Test omitSecondForTimestamp', () => {
+    it('should remove second', () => {
+        expect(omitSecondForTimestamp(0)).toBe(0)
+        expect(omitSecondForTimestamp(59)).toBe(0)
+        expect(omitSecondForTimestamp(60)).toBe(1)
+        expect(omitSecondForTimestamp(600)).toBe(10)
+    })
+});
+
+describe('Test convertToTimestamp', () => {
+    // here is no fraction testing support for ISO 8601
+
+    it('should convert times (according to ISO 8601 time format) to timestamp', () => {
+        const hh = '12'
+        const mm = '30'
+        const ss = '45'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}`)).toBe(12 * 60 * 60)
+        expect(convertToTimestamp(`${hh}${mm}`)).toBe(12 * 60 * 60 + 30 * 60)
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(12 * 60 * 60 + 30 * 60 + 45)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}`)).toBe(12 * 60 * 60 + 30 * 60)
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(12 * 60 * 60 + 30 * 60 + 45)
+    })
+
+    it('should convert beginning time to timestamp correctly', () => {
+        const hh = '00'
+        const mm = '00'
+        const ss = '00'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}`)).toBe(0)
+        expect(convertToTimestamp(`${hh}${mm}`)).toBe(0)
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(0)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}`)).toBe(0)
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(0)
+    })
+
+    it('should convert ending time to timestamp correctly', () => {
+        const hh = '23'
+        const mm = '59'
+        const ss = '59'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(24 * 60 * 60 - 1)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(24 * 60 * 60 - 1)
+    })
+
+    it('should convert midnight to timestamp correctly', () => {
+        const hh = '24'
+        const mm = '00'
+        const ss = '00'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(24 * 60 * 60)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(24 * 60 * 60)
+    })
+
+    it('should convert support 60sec for denote an added leap second.', () => {
+        let hh = '00'
+        let mm = '00'
+        const ss = '60'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(60)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(60)
+
+        mm = '59'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(59 * 60 + 60)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(59 * 60 + 60)
+
+        hh = '23'
+
+        // basic format
+        expect(convertToTimestamp(`${hh}${mm}${ss}`)).toBe(23 * 60 * 60 + 59 * 60 + 60)
+
+        // extended format
+        expect(convertToTimestamp(`${hh}:${mm}:${ss}`)).toBe(23 * 60 * 60 + 59 * 60 + 60)
+    })
+});
+
